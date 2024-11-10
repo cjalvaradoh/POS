806fe7706647d97598a6dd20739c40648440553f
cjalvaradoh <cjalvarado@gmail.com>
2024-11-09 19:16:40 -0600

    Actualizacion 3

--- obj/Debug/net8.0/UspgPOS.AssemblyInfo.cs
+++ obj/Debug/net8.0/UspgPOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UspgPOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e803fabe8de3eff236e420324467966a48c960d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5907cddfbf2b05f1674baf19cc447b1a50dd882")]
 [assembly: System.Reflection.AssemblyProductAttribute("UspgPOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UspgPOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
